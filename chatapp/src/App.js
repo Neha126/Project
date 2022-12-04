e9b95d228de51dd1216326d4a99e685f6f9ee87a
Neha Tomar <tneha5161@gmail.com>
2022-12-04 16:35:51 +0530

    creatin signin/signup page

---
+++ chatapp/src/App.js
@@ -0,0 +1,9 @@
+// import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import "./style.scss";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ chatapp/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <Navbar />
+    </div>
+  );
+};
+export default Sidebar;
